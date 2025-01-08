84f404a5b7fb53cf30803615450a4c2e21dc4bf6
Maarten <maartenzam@gmail.com>
2025-01-08 15:07:32 +0100

    Continuous color scales

---
+++ R/scale_color_wb.R
@@ -0,0 +1,34 @@
+#' Continuous colour scales with WB palettes
+#'
+#'
+#' @param palette The WB color palette to use. One of 'seq', 'seqRev', 'seqB', 'seqY', 'seqP', 'divPosNeg', or 'divLR'
+#' @param direction Sets the order of colors in the scale. 1 is the default, if -1, the order of colors is reversed
+#' @param na.value Color to use for NA values
+#' @param ... Other arguments passed to scale_colour_continuous()
+#'
+#' @return None
+#'
+#' @examples
+#' scale_color_wb(palette = "seq")
+#'
+#' @export
+
+scale_color_wb <- function(...,
+                               palette = "seq",
+                               direction = 1,
+                               na.value = WBCOLORS[["noData"]]) {
+
+  if(palette %in% c('seq', 'seqRev', 'seqB', 'seqY', 'seqP', 'divPosNeg', 'divLR')){
+    pal <- WBPALETTES[[palette]]
+
+    ggplot2::continuous_scale(
+      aesthetics = "colour",
+      scale_name = "wb_continuous",
+      palette = scales::gradient_n_pal(pal),
+      na.value = na.value,
+      ...
+    )
+  } else {
+    cat("The palette does not exist. It should be 'seq', 'seqRev', 'seqB', 'seqY', 'seqP', 'divPosNeg', or 'divLR'")
+  }
+}
